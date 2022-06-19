951cdeffe08a983d8c2c4b94cc6754b2e8c6d9ed
Benjamin Wagner <wagnerb4@students.uni-marburg.de>
2022-06-19 18:23:56 +0200

    fixed voice_of_c_major_seven_octaves test failing

--- src/voice/action/simple_action.rs
+++ src/voice/action/simple_action.rs
@@ -22,31 +22,30 @@ impl<T: notation::Temperament> Action<NeutralActionState> for SimpleAction<T> {
         _state: RefMut<NeutralActionState>,
     ) -> Result<notation::MusicalElement, ActionError> {
         if let Some(pitches) = self.key.get_scale(self.scale_kind, 4, 1, 7 * 7) {
-            let mut char_pos = symbol as u16;
-            let char_pos_cap_a = 'A' as u16;
-            if char_pos < char_pos_cap_a {
-                Err(ActionError::from_generation_error(
+            let char_pos = symbol as u16;
+            const CHAR_POS_CAP_A: u16 = 'A' as u16;
+            const CHAR_POS_CAP_Z: u16 = 'Z' as u16;
+            const CHAR_POS_LOW_A: u16 = 'a' as u16;
+            const CHAR_POS_LOW_W: u16 = 'w' as u16;
+            const CHAR_POS_LOW_X: u16 = 'x' as u16;
+
+            match char_pos {
+                CHAR_POS_LOW_X => Ok(notation::MusicalElement::Rest {
+                    duration: notation::Duration(1),
+                }),
+                CHAR_POS_CAP_A..=CHAR_POS_CAP_Z => Ok(notation::MusicalElement::Note {
+                    pitch: pitches[(char_pos - CHAR_POS_CAP_A) as usize],
+                    duration: notation::Duration(1),
+                    volume: notation::M,
+                }),
+                CHAR_POS_LOW_A..=CHAR_POS_LOW_W => Ok(notation::MusicalElement::Note {
+                    pitch: pitches[(26 + char_pos - CHAR_POS_LOW_A) as usize],
+                    duration: notation::Duration(1),
+                    volume: notation::M,
+                }),
+                _ => Err(ActionError::from_generation_error(
                     &error::MappingError::new(symbol),
-                ))
-            } else {
-                char_pos = char_pos - char_pos_cap_a;
-                if char_pos >= 7 * 7 {
-                    if symbol == 'x' {
-                        Ok(notation::MusicalElement::Rest {
-                            duration: notation::Duration(1),
-                        })
-                    } else {
-                        Err(ActionError::from_generation_error(
-                            &error::MappingError::new(symbol),
-                        ))
-                    }
-                } else {
-                    Ok(notation::MusicalElement::Note {
-                        pitch: pitches[char_pos as usize],
-                        duration: notation::Duration(1),
-                        volume: notation::M,
-                    })
-                }
+                )),
             }
         } else {
             Err(ActionError::from_generation_error(&error::PitchError::new(
